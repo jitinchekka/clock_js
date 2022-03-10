c64570e076881ecb5a918dc4fe96691b31821e45
jitinchekka <jitinchekka2@gmail.com>
2022-03-10 13:17:44 +0530

    added dark mode

--- script.js
+++ script.js
@@ -10,9 +10,28 @@ setInterval(() => {
   });
   time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
   document.getElementById("time").innerHTML = time;
-	document.getElementById("date").innerHTML = date;
-	document.getElementById("utc").innerHTML = a.toLocaleTimeString('en-US', { timeZone: 'UTC' });
+  document.getElementById("date").innerHTML = date;
+  document.getElementById("utc").innerHTML = a.toLocaleTimeString("en-US", {
+    timeZone: "UTC",
+  });
 }, 1000);
+
+let img = document.getElementById("myImage");
+function changeImage() {
+  if (img.src == "https://www.linkpicture.com/q/dark-mode.png") //if light mode
+  {
+    img.src = "https://www.linkpicture.com/q/light-mode.png";
+    document.body.className = "dark-mode";
+    console.log("test if\n");
+    console.log(img.src);
+  } else if (img.src == "https://www.linkpicture.com/q/light-mode.png") //if dark mode
+  {
+    img.src = "https://www.linkpicture.com/q/dark-mode.png";
+    document.body.className = "light-mode";
+    console.log("test else\n");
+    console.log(img.src);
+  }
+}
 
 /* // 	Arrow functions
 //one parameter, one statement
